90a4c4edf9171e027aef16d02baa90eab46a6b50
paulAAV98 <59033526+paulAAV98@users.noreply.github.com>
2023-08-26 16:43:40 -0500

    Configuracion y creacion
    Configuramos la base y creamos la clase proveedor

---
+++ SistemaContable/src/main/java/com/mycompany/sistemacontable/modelo/Proveedor.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.sistemacontable.modelo;
+
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author Usuario
+ */
+@Entity
+
+public class Proveedor {
+    
+}
