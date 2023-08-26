295548f2f3b4478a38c99a6f2fefca17ddc4f2d9
paulAAV98 <59033526+paulAAV98@users.noreply.github.com>
2023-08-26 16:18:57 -0500

    Creamos la clase persona
    La clase persona creada, persistirá en la base

--- SistemaContable/src/main/java/com/mycompany/sistemacontable/modelo/Persona.java
+++ SistemaContable/src/main/java/com/mycompany/sistemacontable/modelo/Persona.java
@@ -4,10 +4,89 @@
  */
 package com.mycompany.sistemacontable.modelo;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  *
  * @author Usuario
  */
+@Entity
 public class Persona {
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="per_id")
+    private int id;
+    
+    @Column(name="per_nombre")
+    private String nombre;
+    
+    @Column(name="per_apellido")
+    private String apellido;
+    
+    @Column(name="per_direccion")
+    private String direccion;
+    
+    @Column(name="per_telefono")
+    private String telefono;
+
+    public Persona() {
+    }
+
+    public Persona(int id, String nombre, String apellido, String direccion, String telefono) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.direccion = direccion;
+        this.telefono = telefono;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    
+    
+    
+    
     
 }
